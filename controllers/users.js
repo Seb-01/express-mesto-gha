caebabef1c4c147952ded15e30614988d777bf6a
Vyacheslav Beketov <beketov-70@bk.ru>
2022-09-10 10:08:15 +0300

    feature: finished fixes for PR14 v.7

--- controllers/users.js
+++ controllers/users.js
@@ -78,11 +78,14 @@ module.exports.login = (req, res, next) => {
 
 // возвращает всех пользователей
 module.exports.getUsers = (req, res, next) => {
-  // если здесь пусто - значит авторизация не прошла
-  if (!req.user) next(new UnAuthoRizedError('Неправильные почта или пароль!'));
-  User.find({})
+  // // если здесь пусто - значит авторизация не прошла
+  // if (!req.user) next(new UnAuthoRizedError('Неправильные почта или пароль!'));
+  // User.find({})
+  //   .then((users) => res.send(users))
+  //   .catch(() => next(new InternalServerError('Произошла внутрення ошибка сервера!')));
+  User.find({}, { password: 0 })
     .then((users) => res.send(users))
-    .catch(() => next(new InternalServerError('Произошла внутрення ошибка сервера!')));
+    .catch(next);
 };
 
 // возвращает пользователя по _id
